4ce81d71b05daaab86a0ccaa2dd51196c8b9b244
Rohit10701 <rohitkumar10701@gmail.com>
2024-03-09 15:51:43 +0530

    style : completed the sqaures coloring

---
+++ src/components/Square.tsx
@@ -0,0 +1,23 @@
+import shadesOfGray from "@/libs/colors";
+import { cn } from "@/libs/utils";
+import React from "react";
+
+type ShadeKey = keyof typeof shadesOfGray;
+interface SquareProps {
+  bgColor: ShadeKey;
+}
+
+const Square = ({ bgColor }: SquareProps) => {
+  console.log("bg-" + bgColor);
+  return (
+    <div
+      className={"bg-" + shadesOfGray[bgColor] + " min-w-4 min-h-4 w-4 h-4 flex justify-center items-center rounded-sm shadow-sm shadow-gray m-[1px] hover:opacity-50 hover:cursor-pointer  transition duration-2000 ease-in-out"}
+    >
+      <div className=" min-w-1 min-h-1 w-1 h-1 bg-white blur-sm ">
+      &nbsp;
+      </div>
+    </div>
+  );
+};
+
+export default Square;
---
+++ src/libs/colors.ts
@@ -0,0 +1,15 @@
+const shadesOfGray = {
+    1: "[#FFFFFF]", // White
+    2: "[#E6E6E6]",
+    3: "[#CCCCCC]",
+    4: "[#B3B3B3]",
+    5: "[#999999]",
+    6: "[#808080]",
+    7: "[#666666]",
+    8: "[#4D4D4D]",
+    9: "[#333333]",
+    10: "[#1A1A1A]",
+    11: "[#000000]", // Black
+  };
+
+  export default shadesOfGray
